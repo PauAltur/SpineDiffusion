af08a3e6876e53ac9e6e5d6d2dbdd46db5d26188
paltur <paltur@student.ethz.ch>
2024-06-12 16:49:33 +0200

    Datamodule v1

--- spinediffusion/datamodule/datamodule.py
+++ spinediffusion/datamodule/datamodule.py
@@ -1,7 +1,9 @@
 import glob
 import json
+import open3d as o3d
 from datetime import datetime
 from pathlib import Path
+from tqdm import tqdm
 
 import numpy as np
 import pandas as pd
@@ -9,9 +11,18 @@ import pytorch_lightning as pl
 from natsort import natsorted
 from torch.utils.data import DataLoader, Dataset
 from torchvision.transforms import v2
+from typing import Optional
+from .transforms.normalizing import Normalize
+from .transforms.resampling import Resample3DCurve
+from .transforms.cropping import Crop
+from .transforms.centering import Center
 
 TRANSFORMS = {
     "ToTensor": v2.ToTensor,
+    "Normalize": Normalize,
+    "Resample3DCurve": Resample3DCurve,
+    "Crop": Crop,
+    "Center": Center,
 }
 
 
@@ -26,154 +37,113 @@ class SpineDataModule(pl.LightningDataModule):
         self,
         data_dir: str,
         batch_size: int,
-        train_ratio: float | None,
-        val_ratio: float | None,
-        test_ratio: float | None,
-        train_indices: list | None,
-        val_indices: list | None,
-        test_indices: list | None,
         transform_args: dict,
-        surface_rotation: bool,
+        train_fraction: Optional[float] = None,
+        val_fraction: Optional[float] = None,
+        test_fraction: Optional[float] = None,
+        train_indices: Optional[list] = None,
+        val_indices: Optional[list] = None,
+        test_indices: Optional[list] = None,
+        n_subjects: Optional[float] = None,
     ):
         """_summary_
 
         Args:
             data_dir (str): _description_
             batch_size (int): _description_
-            train_ratio (float | None): _description_
-            val_ratio (float | None): _description_
-            test_ratio (float | None): _description_
-            train_indices (list | None): _description_
-            val_indices (list | None): _description_
-            test_indices (list | None): _description_
             transform_args (dict): _description_
-            surface_rotation (bool): _description_
-        """
+            train_fraction (Optional[float], optional): _description_. Defaults to None.
+            val_fraction (Optional[float], optional): _description_. Defaults to None.
+            test_fraction (Optional[float], optional): _description_. Defaults to None.
+            train_indices (Optional[list], optional): _description_. Defaults to None.
+            val_indices (Optional[list], optional): _description_. Defaults to None.
+            test_indices (Optional[list], optional): _description_. Defaults to None.
+            n_subjects (Optional[float], optional): _description_. Defaults to None.
+        """        
+        
         super().__init__()
-        self.data_dir = data_dir
+        self.data_dir = Path(data_dir)
         self.batch_size = batch_size
-        self.train_ratio = train_ratio
-        self.val_ratio = val_ratio
-        self.test_ratio = test_ratio
+        self.train_fraction = train_fraction
+        self.val_fraction = val_fraction
+        self.test_fraction = test_fraction
         self.train_indices = train_indices
         self.val_indices = val_indices
         self.test_indices = test_indices
         self.transform_args = transform_args
-        self.surface_rotation = surface_rotation
+        self.data = {"backscans": {},"metadata" : {}}
+        self.n_subjects = n_subjects
 
-    def setup(self, stage: str | None):
+    def setup(self, stage: Optional[str]):
         """_summary_
 
         Args:
             stage (str | None): _description_
         """
         self._parse_datapaths()
-        self._load_metadata()
-        self._load_scandata()
+        self._load_data("metadata", self.dirs_metadata)
+        self._load_data("backscans", self.dirs_backscan)
         self._preprocess_data()
-        self._check_split_args()
         self._split_data()
 
     def _parse_datapaths(self):
         """_summary_"""
-        assert Path(self.data_dir).exists(), "Data directory does not exist."
-        data_dir = Path(self.data_dir)
+        print(f"Parsing data paths from {self.data_dir}...")
 
-        dirs_backscan = natsorted(
-            glob.glob(str(data_dir / "**/*_backscan.ply"))
-        )
-        dirs_metadata = natsorted(
-            glob.glob(str(data_dir / "**/*_metadata.json"))
-        )
+        assert self.data_dir.exists(), "Data directory does not exist."
 
-        self.metadata = pd.DataFrame(
-            list(zip(dirs_backscan, dirs_metadata)),
-            columns=['dirs_backscan', 'dirs_metadata']
-        )
+        backscan_dir = str(self.data_dir / "**" / f"*processed.ply")
+        metadata_dir = str(self.data_dir / "**" / f"*processed.json")
 
-    def _load_metadata(self):
-        """_summary_"""
-        dirs_metadata = self.metadata['dirs_metadata'].tolist()
+        # making every path string a Path object is useful for cross-OS compatibility
+        self.dirs_backscan = [
+            Path(path) for path in natsorted(glob.glob(backscan_dir))
+        ]
+        self.dirs_metadata = [
+            Path(path) for path in natsorted(glob.glob(metadata_dir))
+        ]
 
-        subject_idx = np.empty(len(dirs_metadata), dtype=object)
-        age_list = np.zeros(len(dirs_metadata), dtype=int)
-        gender = np.empty((len(dirs_metadata)), dtype=str)
-        rec_time = np.empty((len(dirs_metadata)), dtype=datetime)
-        esl = np.zeros(len(dirs_metadata), dtype=object)
-        isl = np.zeros(len(dirs_metadata), dtype=object)
-        fix_points = np.zeros(len(dirs_metadata), dtype=dict)
-        apex_points = np.zeros(len(dirs_metadata), dtype=object)
-        if self.surface_rotation:
-            surface_rotation = np.zeros(len(dirs_metadata), dtype=float)
+    def _load_data(self, file_ext: str, data_dirs: list):
+        """_summary_
 
-        for idx, dir in enumerate(dirs_metadata):
-            # Opening JSON file
-            f = open(dir)
-            data = json.load(f)
-
-            # some age entries are wrong, they need to be corrected
-            if isinstance(data['age'], str):
-                converted_date = datetime.strptime(data['age'], "%d.%m.%Y").year
-                year_diff = converted_date - 2000
-                age_list[idx] = year_diff
-            else:
-                age_list[idx] = data['age']
-
-            subject_idx[idx] = data['subject_index']
-            gender[idx] = data['gender']
-            rec_time[idx] = data['recording_datetime']
-            esl[idx] = np.array(data['esl_formetric'])
-            isl[idx] = np.array(data['isl_formetric'])
-            fix_points[idx] = data['fix_pts']
-            apex_points[idx] = data['apex_pts']
-            if self.surface_rotation:
-                surface_rotation[idx] = data['surface_rotation_rms_deg']
-
-            # Closing file
-            f.close()
-
-        # convert "w" to 0 and "m" to 1 in gender
-        gender_list = np.asarray([0 if x == "w" else 1 for x in gender])
-
-        self.metadata.insert(0, 'subject_id', subject_idx)
-        self.metadata = self.metadata.assign(gender=gender_list, age=age_list, rec_time=rec_time, esl=esl, isl=isl, fix_points=fix_points,
-                                apex_points=apex_points)
-
-        if self.surface_rotation:
-            self.metadata['surface_rotation'] = surface_rotation
-
-        self.metadata['rec_time'] = pd.to_datetime(self.metadata['rec_time'], format='%d-%b-%Y %H:%M:%S')
-    
-    def _load_scandata(self):
-        """_summary_"""
-        pass
-
+        Args:
+            file (str): _description_
+            data_dirs (list): _description_
+        """        
+        print(f"Loading {file_ext}...")
+        for path in tqdm(data_dirs[:self.n_subjects]):
+            unique_id = path.parts[-2]
+            if path.suffix == ".ply":
+                self.data[file_ext][unique_id] = o3d.io.read_point_cloud(str(path))
+            elif path.suffix == ".json":
+                with open(path, "r") as f:
+                    self.data[file_ext][unique_id] = json.load(f)
 
     def _check_split_args(self):
         """_summary_
         """
-        ratio_args = [self.train_ratio, self.val_ratio, self.test_ratio]
+        fraction_args = [self.train_fraction, self.val_fraction, self.test_fraction]
         indices_args = [self.train_indices, self.val_indices, self.test_indices]
 
-        ratio_bool = np.all([ratio is not None for ratio in ratio_args])
+        fraction_bool = np.all([fraction is not None for fraction in fraction_args])
         indices_bool = np.all(
             [indices is not None for indices in indices_args]
         )
 
-        msg = "Either ratios or indices should be provided, not both."
-        assert np.xor(ratio_bool, indices_bool), msg
+        msg = "Either fractions or indices should be provided, not both."
+        assert np.xor(fraction_bool, indices_bool), msg
 
-        if ratio_bool:
-            msg = "Sum of ratios should be equal to 1."
-            assert np.sum(ratio_args) == 1, msg
+        if fraction_bool:
+            msg = "Sum of fractions should be equal to 1."
+            assert np.sum(fraction_args) == 1, msg
             self.train_indices = np.random.choice(
                 np.arange(len(self.data)),
-                size=int(self.train_ratio * len(self.data)),
+                size=int(self.train_fraction * len(self.data)),
                 replace=False,
             )
             self.val_indices = np.random.choice(
                 np.setdiff1d(np.arange(len(self.data)), self.train_indices),
-                size=int(self.val_ratio * len(self.data)),
+                size=int(self.val_fraction * len(self.data)),
                 replace=False,
             )
             self.test_indices = np.setdiff1d(
@@ -183,6 +153,7 @@ class SpineDataModule(pl.LightningDataModule):
 
     def _preprocess_data(self):
         """_summary_"""
+        print("Preprocessing data...")
         transforms = []
         for i in range(len(self.transform_args)):
             for key, value in self.transform_args.items():
@@ -193,6 +164,8 @@ class SpineDataModule(pl.LightningDataModule):
 
     def _split_data(self):
         """_summary_"""
+        print("Splitting data...")
+        self._check_split_args()
         self.train_data = Dataset(self.data[self.train_indices])
         self.val_data = Dataset(self.data[self.val_indices])
         self.test_data = Dataset(self.data[self.test_indices])
---
+++ spinediffusion/datamodule/transforms/cropping.py
@@ -0,0 +1,8 @@
+import torch.nn as nn
+
+class Crop(nn.Module):
+    def __init__(self, **kwargs):
+        super().__init__()
+
+    def forward(self, data):
+        return data
